c8342546554788b55f6a3d1a20aeffb559253093
Черных Александр <razor01478920@mail.ru>
2024-06-12 16:13:00 +0300

    Исправил публикуемую версию

--- kotlinflowextended/build.gradle.kts
+++ kotlinflowextended/build.gradle.kts
@@ -37,7 +37,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.ravenzip.kotlinflowextended"
             artifactId = "kotlinflowextended"
-            version = "1.0.0"
+            version = "1.0.1"
 
             afterEvaluate { from(components["release"]) }
         }
