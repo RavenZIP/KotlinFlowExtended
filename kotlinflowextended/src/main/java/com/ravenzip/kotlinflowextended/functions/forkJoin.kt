754d45f569ac56e0bfa4bbdcd3046572f906dafa
Черных Александр <razor01478920@mail.ru>
2024-12-07 09:29:13 +0300

    Переписал ForkJoin + добавил перегрузку с vararg вместо list

--- kotlinflowextended/src/main/java/com/ravenzip/kotlinflowextended/functions/forkJoin.kt
+++ kotlinflowextended/src/main/java/com/ravenzip/kotlinflowextended/functions/forkJoin.kt
@@ -1,12 +1,14 @@
 package com.ravenzip.kotlinflowextended.functions
 
 import com.ravenzip.kotlinflowextended.models.FlowOIndexedObject
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Semaphore
 
 /**
  * [forkJoin]
@@ -16,42 +18,62 @@ import kotlinx.coroutines.launch
  *
  * @param flows список потоков
  * @param flowLimit количество одновременно работающих потоков
- * @param delay задержка перед переходом к следующей итерации
  * @return [Flow]
  */
-fun <T> forkJoin(flows: List<Flow<T>>, flowLimit: Int = 3, delay: Long = 100): Flow<List<T>> =
-    flow {
-        coroutineScope {
-            val flowsCount = flows.count()
-            val results = mutableListOf<FlowOIndexedObject<T>>()
-            var availableFlowCount = flowLimit
-            var itemIndex = 0
-            var isNotComplete = true
+fun <T> forkJoin(flows: List<Flow<T>>, flowLimit: Int = 3): Flow<List<T>> = flow {
+    coroutineScope {
+        // Семафор для ограничения количества одновременно работающих потоков
+        val semaphore = Semaphore(flowLimit)
+        val deferredResults = mutableListOf<Deferred<FlowOIndexedObject<T>>>()
 
-            while (isNotComplete) {
-                if (itemIndex <= flowsCount - 1 && availableFlowCount <= flowLimit) {
-                    val rightLimit = availableFlowCount + itemIndex - 1
-                    for (i in itemIndex..rightLimit) {
-                        launch(Dispatchers.Main) {
-                            flows[i].collect { value ->
-                                results.add(FlowOIndexedObject.create(value, i))
-                                availableFlowCount += 1
-                                if (
-                                    results.count() == flowsCount &&
-                                    availableFlowCount == flowsCount
-                                ) {
-                                    isNotComplete = false
-                                }
-                            }
-                        }
-
-                        availableFlowCount -= 1
-                        itemIndex = i + 1
-                    }
+        flows.forEachIndexed { index, flow ->
+            semaphore.acquire() // Ограничиваем количество потоков
+            val deferred = async {
+                try {
+                    val result = flow.first() // Берем только первый элемент (как в RxJS forkJoin)
+                    FlowOIndexedObject.create(result, index)
+                } finally {
+                    semaphore.release() // Освобождаем место в семафоре
                 }
-                delay(delay)
             }
-
-            emit(results.sortedBy { it.index }.map { it.value })
+            deferredResults.add(deferred)
         }
+
+        // Ждем завершения всех задач и сортируем результаты
+        val results = deferredResults.awaitAll().sortedBy { it.index }.map { it.value }
+        emit(results)
     }
+}
+
+/**
+ * [forkJoin]
+ *
+ * Предназначен для параллельного выполнения запросов и выдачи результатов списком в том порядке, в
+ * котором они были переданы
+ *
+ * @param flows список потоков
+ * @param flowLimit количество одновременно работающих потоков
+ * @return [Flow]
+ */
+fun <T> forkJoin(vararg flows: Flow<T>, flowLimit: Int = 3): Flow<List<T>> = flow {
+    coroutineScope {
+        val semaphore = Semaphore(flowLimit)
+        val deferredResults = mutableListOf<Deferred<FlowOIndexedObject<T>>>()
+
+        flows.forEachIndexed { index, flow ->
+            semaphore.acquire()
+            val deferred = async {
+                try {
+                    val result = flow.first()
+                    FlowOIndexedObject(result, index)
+                } finally {
+                    semaphore.release()
+                }
+            }
+            deferredResults.add(deferred)
+        }
+
+        val results = deferredResults.awaitAll().sortedBy { it.index }.map { it.value }
+        emit(results)
+    }
+}
