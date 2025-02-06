ae49ad1ddc0149da915ebcaad9216a9684c33445
Черных Александр <razor01478920@mail.ru>
2025-02-06 08:16:42 +0300

    Явно указываем типы для операторов

--- kotlinflowextended/src/main/java/com/ravenzip/kotlinflowextended/functions/filter.kt
+++ kotlinflowextended/src/main/java/com/ravenzip/kotlinflowextended/functions/filter.kt
@@ -4,20 +4,20 @@ import com.ravenzip.kotlinflowextended.models.FlowNotification
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.transform
 
-fun <T> Flow<T>.filterNull() =
+fun <T> Flow<T>.filterNull(): Flow<T?> =
     this.transform { value -> if (value == null) return@transform emit(value) }
 
-fun <T> Flow<FlowNotification<T>>.filterNextNotification() =
+fun <T> Flow<FlowNotification<T>>.filterNextNotification(): Flow<FlowNotification.Next<T>> =
     this.transform { notification ->
         if (notification is FlowNotification.Next) return@transform emit(notification)
     }
 
-fun <T> Flow<FlowNotification<T>>.filterErrorNotification() =
+fun <T> Flow<FlowNotification<T>>.filterErrorNotification(): Flow<FlowNotification.Error> =
     this.transform { notification ->
         if (notification is FlowNotification.Error) return@transform emit(notification)
     }
 
-fun <T> Flow<FlowNotification<T>>.filterCompleteNotification() =
+fun <T> Flow<FlowNotification<T>>.filterCompleteNotification(): Flow<FlowNotification.Complete> =
     this.transform { notification ->
         if (notification is FlowNotification.Complete) return@transform emit(notification)
     }
